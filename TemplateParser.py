3883a5c5ffa29ed94f2e741f9e4d1ce577120201
liambabe <liambabes13@gmail.com>
2019-12-17 09:10:23 +1300

    Implementing type hinting csv file extensions

--- TemplateParser.py
+++ TemplateParser.py
@@ -1,14 +1,18 @@
+from typing import List
+
 class TemplateParser:
 
-    tags = []
-    templateFolder = ""
-    templatePath = ""
+    tags: List[str]
+    templateFolder: str
+    templatePath: str
+    fileExtension: str
 
     def __init__(self):
         self.templateFolder = "Templates/"
+        self.fileExtension = ".csv"
 
     def parse(self, fileName):
-        self.templatePath = self.templateFolder + fileName
+        self.templatePath = self.templateFolder + fileName + self.fileExtension
         file = open(self.templatePath, "r")
         self.tags = file.read().split(",")
 
